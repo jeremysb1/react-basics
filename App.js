cf6679e7377e72865c37952cc898532f02e595e0
Jeremy Hall <21281011+jeremysb1@users.noreply.github.com>
2021-02-22 15:02:58 -0500

    adding map function to display data

--- App.js
+++ App.js
@@ -7,18 +7,30 @@ class App extends Component {
     super();
 
     this.state = {
-      string: 'Hello Jeremy'
-    }
+      monsters: [
+        {
+          name: 'Frankenstein',
+          id: 'asc1'
+        },
+        {
+          name: 'Dracula',
+          id: 'asr2'
+        },
+        {
+          name: 'Zombie',
+          id: 'asw1'
+        }
+      ]
+    };
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{ this.state.string }</p>
-          <button onClick={() => this.setState({ string: 'Hello Stranger' })}>Change Text</button>
-        </header>
+        {
+          this.state.monsters.map(monster => 
+            <h1 key={monster.id}> { monster.name } </h1>)
+        }
       </div>
     );
   }
